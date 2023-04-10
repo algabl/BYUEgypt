86b3f4ca7f4d9e135392ef77fd2ec80d9be239c7
algabl <103858110+algabl@users.noreply.github.com>
2023-04-10 16:33:36 -0600

    admin controller updates

--- Program.cs
+++ Program.cs
@@ -33,6 +33,11 @@ builder.Services.AddAuthentication().AddMicrosoftAccount(microsoftOptions =>
     microsoftOptions.ClientId = configuration["Authentication:Microsoft:ClientId"];
     microsoftOptions.ClientSecret = configuration["Authentication:Microsoft:ClientSecret"];
 });
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = configuration["Authentication:Google:ClientID"];
+    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+});
 
 
 var app = builder.Build();
