9d18efde4eae89d03c5d668a8843a4a08ecd9e81
ckearl <c2_kearl@yahoo.com>
2023-04-12 13:53:07 -0600

    api route and most of the form

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -58,15 +58,6 @@ public class HomeController : Controller
         return View();
     }
     
-    public IActionResult SupAnalysis()
-    {
-        return View();
-    }
-    
-    public IActionResult UnSupAnalysis()
-    {
-        return View();
-    }
     
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
